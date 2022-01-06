d8dd27151c7d1897a7499c73b6a5de3353fe17e1
Alexey9719 <93517564+Alexey9719@users.noreply.github.com>
2022-01-06 16:51:46 +0200

    Update TitlesPanel.java

--- source_files/TitlesPanel.java
+++ source_files/TitlesPanel.java
@@ -1,71 +1,68 @@
-import java.awt.Insets;
 import java.awt.Dimension;
-import java.awt.geom.AffineTransform;
-import java.awt.Stroke;
-import java.awt.RenderingHints;
 import java.awt.Graphics;
-import java.awt.event.ActionEvent;
-import javax.swing.Timer;
 import java.awt.Graphics2D;
+import java.awt.Insets;
+import java.awt.RenderingHints;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.geom.AffineTransform;
 import javax.swing.JPanel;
+import javax.swing.Timer;
 
-// 
-// Decompiled by Procyon v0.5.36
-// 
+public class TitlesPanel extends JPanel implements ActionListener {
+   private Graphics2D g2d;
+   private Timer animation;
+   private boolean is_done = true;
+   private int start_angle = 0;
+   private int shape;
 
-public class TitlesPanel extends JPanel implements ActionListener
-{
-    private Graphics2D g2d;
-    private Timer animation;
-    private boolean is_done;
-    private int start_angle;
-    private int shape;
-    
-    public TitlesPanel(final int _shape) {
-        this.start_angle = 0;
-        this.is_done = true;
-        this.shape = _shape;
-        (this.animation = new Timer(50, this)).setInitialDelay(50);
-        this.animation.start();
-    }
-    
-    @Override
-    public void actionPerformed(final ActionEvent arg0) {
-        if (this.is_done) {
-            this.repaint();
-        }
-    }
-    
-    private void doDrawing(final Graphics g) {
-        this.is_done = false;
-        (this.g2d = (Graphics2D)g).setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-        final Dimension size = this.getSize();
-        final Insets insets = this.getInsets();
-        final int w = size.width - insets.left - insets.right;
-        final int h = size.height - insets.top - insets.bottom;
-        final ShapeFactory shape = new ShapeFactory(this.shape);
-        this.g2d.setStroke(shape.stroke);
-        this.g2d.setPaint(shape.paint);
-        double angle = this.start_angle++;
-        if (this.start_angle > 360) {
-            this.start_angle = 0;
-        }
-        final double dr = 90.0 / (w / (shape.width * 1.5));
-        for (int j = shape.height; j < h; j += (int)(shape.height * 1.5)) {
-            for (int i = shape.width; i < w; i += (int)(shape.width * 1.5)) {
-                angle = ((angle > 360.0) ? 0.0 : (angle + dr));
-                final AffineTransform transform = new AffineTransform();
-                transform.translate(i, j);
-                transform.rotate(Math.toRadians(angle));
-                this.g2d.draw(transform.createTransformedShape(shape.shape));
-            }
-        }
-        this.is_done = true;
-    }
-    
-    public void paintComponent(final Graphics g) {
-        super.paintComponent(g);
-        this.doDrawing(g);
-    }
+   public TitlesPanel(int _shape) {
+      this.shape = _shape;
+      this.animation = new Timer(50, this);
+      this.animation.setInitialDelay(50);
+      this.animation.start();
+   }
+
+   public void actionPerformed(ActionEvent arg0) {
+      if (this.is_done) {
+         this.repaint();
+      }
+
+   }
+
+   private void doDrawing(Graphics g) {
+      this.is_done = false;
+      this.g2d = (Graphics2D)g;
+      this.g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+      Dimension size = this.getSize();
+      Insets insets = this.getInsets();
+      int w = size.width - insets.left - insets.right;
+      int h = size.height - insets.top - insets.bottom;
+      ShapeFactory shape = new ShapeFactory(this.shape);
+      this.g2d.setStroke(shape.stroke);
+      this.g2d.setPaint(shape.paint);
+      double angle = (double)(this.start_angle++);
+      if (this.start_angle > 360) {
+         this.start_angle = 0;
+      }
+
+      double dr = 90.0D / ((double)w / ((double)shape.width * 1.5D));
+
+      for(int j = shape.height; j < h; j = (int)((double)j + (double)shape.height * 1.5D)) {
+         for(int i = shape.width; i < w; i = (int)((double)i + (double)shape.width * 1.5D)) {
+            angle = angle > 360.0D ? 0.0D : angle + dr;
+            AffineTransform transform = new AffineTransform();
+            transform.translate((double)i, (double)j);
+            transform.rotate(Math.toRadians(angle));
+            this.g2d.draw(transform.createTransformedShape(shape.shape));
+         }
+      }
+
+      this.is_done = true;
+   }
+
+   public void paintComponent(Graphics g) {
+      super.paintComponent(g);
+      this.doDrawing(g);
+   }
 }
