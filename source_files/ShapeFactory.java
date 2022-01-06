e8c35317d6e45dbc646399e978938ba806c1e8dd
Alexey9719 <93517564+Alexey9719@users.noreply.github.com>
2022-01-06 16:51:43 +0200

    Update ShapeFactory.java

--- source_files/ShapeFactory.java
+++ source_files/ShapeFactory.java
@@ -1,101 +1,87 @@
-import java.awt.geom.Point2D;
-import java.awt.GradientPaint;
-import java.awt.Color;
-import java.awt.geom.Arc2D;
-import java.awt.geom.GeneralPath;
-import java.awt.geom.Rectangle2D;
-import java.awt.Point;
-import java.awt.Paint;
 import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.GradientPaint;
+import java.awt.Paint;
+import java.awt.Point;
 import java.awt.Shape;
+import java.awt.geom.GeneralPath;
+import java.awt.geom.Rectangle2D.Double;
 
-// 
-// Decompiled by Procyon v0.5.36
-// 
+public class ShapeFactory {
+   public Shape shape;
+   public BasicStroke stroke = new BasicStroke(3.0F);
+   public Paint paint;
+   public int width = 25;
+   public int height = 25;
 
-public class ShapeFactory
-{
-    public Shape shape;
-    public BasicStroke stroke;
-    public Paint paint;
-    public int width;
-    public int height;
-    
-    public ShapeFactory(final int shape_type) {
-        this.width = 25;
-        this.height = 25;
-        this.stroke = new BasicStroke(3.0f);
-        switch (shape_type / 10) {
-            case 1: {
-                this.shape = createStar(3, new Point(0, 0), this.width / 2.0, this.width / 2.0);
-                break;
-            }
-            case 3: {
-                this.shape = createStar(5, new Point(0, 0), this.width / 2.0, this.width / 4.0);
-                break;
-            }
-            case 5: {
-                this.shape = new Rectangle2D.Double(-this.width / 2.0, -this.height / 2.0, this.width, this.height);
-                break;
-            }
-            case 7: {
-                final GeneralPath path = new GeneralPath();
-                final double tmp_height = Math.sqrt(2.0) / 2.0 * this.height;
-                path.moveTo(-this.width / 2, -tmp_height);
-                path.lineTo(0.0, -tmp_height);
-                path.lineTo(this.width / 2, tmp_height);
-                path.closePath();
-                this.shape = path;
-                break;
-            }
-            case 9: {
-                this.shape = new Arc2D.Double(-this.width / 2.0, -this.height / 2.0, this.width, this.height, 30.0, 300.0, 2);
-                break;
-            }
-            default: {
-                throw new Error("type is nusupported");
-            }
-        }
-        switch (shape_type % 10) {
-            case 1: {
-                this.stroke = new BasicStroke(3.0f);
-                break;
-            }
-            case 3: {
-                break;
-            }
-            case 4: {
-                this.stroke = new BasicStroke(7.0f);
-                break;
-            }
-            case 7: {
-                this.paint = new GradientPaint((float)(-this.width), (float)(-this.height), Color.white, (float)this.width, (float)this.height, Color.gray, true);
-                break;
-            }
-            case 8: {
-                this.paint = Color.red;
-                break;
-            }
-            default: {
-                throw new Error("type is nusupported");
-            }
-        }
-    }
-    
-    private static Shape createStar(final int arms, final Point center, final double rOuter, final double rInner) {
-        final double angle = 3.141592653589793 / arms;
-        final GeneralPath path = new GeneralPath();
-        for (int i = 0; i < 2 * arms; ++i) {
-            final double r = ((i & 0x1) == 0x0) ? rOuter : rInner;
-            final Point2D.Double p = new Point2D.Double(center.x + Math.cos(i * angle) * r, center.y + Math.sin(i * angle) * r);
-            if (i == 0) {
-                path.moveTo(p.getX(), p.getY());
-            }
-            else {
-                path.lineTo(p.getX(), p.getY());
-            }
-        }
-        path.closePath();
-        return path;
-    }
+   public ShapeFactory(int shape_type) {
+      switch(shape_type / 10) {
+      case 1:
+         this.shape = createStar(3, new Point(0, 0), (double)this.width / 2.0D, (double)this.width / 2.0D);
+         break;
+      case 2:
+      case 4:
+      case 6:
+      case 8:
+      default:
+         throw new Error("type is nusupported");
+      case 3:
+         this.shape = createStar(5, new Point(0, 0), (double)this.width / 2.0D, (double)this.width / 4.0D);
+         break;
+      case 5:
+         this.shape = new Double((double)(-this.width) / 2.0D, (double)(-this.height) / 2.0D, (double)this.width, (double)this.height);
+         break;
+      case 7:
+         GeneralPath path = new GeneralPath();
+         double tmp_height = Math.sqrt(2.0D) / 2.0D * (double)this.height;
+         path.moveTo((double)(-this.width / 2), -tmp_height);
+         path.lineTo(0.0D, -tmp_height);
+         path.lineTo((double)(this.width / 2), tmp_height);
+         path.closePath();
+         this.shape = path;
+         break;
+      case 9:
+         this.shape = new java.awt.geom.Arc2D.Double((double)(-this.width) / 2.0D, (double)(-this.height) / 2.0D, (double)this.width, (double)this.height, 30.0D, 300.0D, 2);
+      }
+
+      switch(shape_type % 10) {
+      case 1:
+         this.stroke = new BasicStroke(3.0F);
+         break;
+      case 2:
+      case 5:
+      case 6:
+      default:
+         throw new Error("type is nusupported");
+      case 3:
+         break;
+      case 4:
+         this.stroke = new BasicStroke(7.0F);
+         break;
+      case 7:
+         this.paint = new GradientPaint((float)(-this.width), (float)(-this.height), Color.white, (float)this.width, (float)this.height, Color.gray, true);
+         break;
+      case 8:
+         this.paint = Color.red;
+      }
+
+   }
+
+   private static Shape createStar(int arms, Point center, double rOuter, double rInner) {
+      double angle = 3.141592653589793D / (double)arms;
+      GeneralPath path = new GeneralPath();
+
+      for(int i = 0; i < 2 * arms; ++i) {
+         double r = (i & 1) == 0 ? rOuter : rInner;
+         java.awt.geom.Point2D.Double p = new java.awt.geom.Point2D.Double((double)center.x + Math.cos((double)i * angle) * r, (double)center.y + Math.sin((double)i * angle) * r);
+         if (i == 0) {
+            path.moveTo(p.getX(), p.getY());
+         } else {
+            path.lineTo(p.getX(), p.getY());
+         }
+      }
+
+      path.closePath();
+      return path;
+   }
 }
